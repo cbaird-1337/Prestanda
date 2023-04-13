814362cfd2ddacc5cffdf67ad85b3fbe5293c09f
colin baird <colinbaird89@gmail.com>
2023-04-13 09:46:22 -0500

    updated stop words function & openai system prompt

--- interview-bot-backend/server.js
+++ interview-bot-backend/server.js
@@ -18,15 +18,15 @@ const accountProfileController = require('./account_profiles/accountProfileContr
 
 // Configure AWS SDK and initialize S3 instance
 AWS.config.update({
-  accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
-  region: process.env.REACT_APP_AWS_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
 });
 const s3 = new AWS.S3();
 
 // Configure OpenAI API client
 const configuration = new Configuration({
-  apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
@@ -39,31 +39,13 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 //defining stopwords to be removed
-const stopWords = [
-  'a', 'about', 'above', 'after', 'again', 'against', 'ain', 'all', 'am', 'an', 'and', 'any', 'are', 'aren', 'as', 'at',
-  'be', 'because', 'been', 'before', 'being', 'below', 'between', 'both', 'but', 'by',
-  'can', 'couldn', 'd', 'did', 'didn', 'do', 'does', 'doesn', 'doing', 'don', 'down', 'during',
-  'each',
-  'few', 'for', 'from', 'further',
-  'had', 'hadn', 'has', 'hasn', 'have', 'haven', 'having', 'he', 'her', 'here', 'hers', 'herself', 'him', 'himself', 'his', 'how',
-  'i', 'if', 'in', 'into', 'is', 'isn', 'it', 'its', 'itself',
-  'll',
-  'm', 'ma', 'me', 'mightn', 'more', 'most', 'mustn', 'my', 'myself',
-  'needn', 'no', 'nor', 'not', 'now',
-  'o', 'of', 'off', 'on', 'once', 'only', 'or', 'other', 'our', 'ours', 'ourselves', 'out', 'over', 'own',
-  're',
-  's', 'same', 'shan', 'she', 'should', 'shouldn', 'so', 'some', 'such',
-  't', 'than', 'that', 'the', 'their', 'theirs', 'them', 'themselves', 'then', 'there', 'these', 'they', 'this', 'those', 'through', 'to', 'too',
-  'under', 'until', 'up',
-  've', 'very',
-  'was', 'wasn', 'we', 'were', 'weren', 'what', 'when', 'where', 'which', 'while', 'who', 'whom', 'why', 'will', 'with', 'won', 'wouldn',
-  'y', 'you', 'your', 'yours', 'yourself', 'yourselves'
-];
+const natural = require('natural');
+const tokenizer = new natural.WordTokenizer();
 
 // Function to remove stop words and extra whitespace
 function removeStopWordsAndExtraWhitespace(text) {
-  const words = text.split(/\s+/);
-  const filteredWords = words.filter(word => !stopWords.includes(word.toLowerCase()));
+  const words = tokenizer.tokenize(text);
+  const filteredWords = words.filter(word => !natural.stopwords.includes(word.toLowerCase()));
   return filteredWords.join(' ').replace(/\s\s+/g, ' ');
 }
 
@@ -127,13 +109,13 @@ async function readAndExtractTextFromS3(s3Bucket, s3Key) {
 
 // Function for ChatGPT to generate interview questions based on resume and job description uploads
 async function generateQuestions(text, role, numberOfQuestions) {
-  const prompt = `Please generate ${numberOfQuestions} first call phone screening interview questions, with a maximum length of 20 words per question, that focus on evaluating career experience, job fit, and specific examples for the ${role} position. Ensure that at least one or two questions are behavioral, such as "tell me about a time when you faced a challenge and how you overcame it". Consider the following job description and resume details while creating the questions:\n\n${text}\n`;
+  const prompt = `Please generate ${numberOfQuestions} first call phone screening interview questions that focus on evaluating career experience, job fit, and specific examples for the ${role} position. Consider the following job description and resume details while creating the questions:\n\n${text}\n`;
   const completion = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     messages: [
       {
         role: "system",
-        content: "You are a helpful recruiting assistant that generates interview questions based on the given text."
+        content: "You are a helpful recruiting assistant that generates interview questions based on input from a user's resume and job description. Each question should have a maximum length of 20 words per question that focus on evaluating career experience, job fit, and specific examples for the position. Ensure that at least one of these questions are behavioral, such as tell me about a time when you faced a challenge and how you overcame it"
       },
       {
         role: "user",
@@ -171,12 +153,12 @@ app.post('/upload', upload.fields([{ name: 'resume' }, { name: 'jobDescription' 
     // Upload the files to the S3 bucket
     const resumeS3Key = `resumes/${resumeFile.originalname}`;
     const jobDescriptionS3Key = `job_descriptions/${jobDescriptionFile.originalname}`;
-    await uploadToS3(resumeFile, process.env.REACT_APP_S3_BUCKET_NAME, resumeS3Key);
-    await uploadToS3(jobDescriptionFile, process.env.REACT_APP_S3_BUCKET_NAME, jobDescriptionS3Key);
+    await uploadToS3(resumeFile, process.env.S3_BUCKET_NAME, resumeS3Key);
+    await uploadToS3(jobDescriptionFile, process.env.S3_BUCKET_NAME, jobDescriptionS3Key);
 
     // Read and extract text from the uploaded files
-    const resumeText = removeStopWordsAndExtraWhitespace(await readAndExtractTextFromS3(process.env.REACT_APP_S3_BUCKET_NAME, resumeS3Key));
-    const jobDescriptionText = removeStopWordsAndExtraWhitespace(await readAndExtractTextFromS3(process.env.REACT_APP_S3_BUCKET_NAME, jobDescriptionS3Key));
+    const resumeText = removeStopWordsAndExtraWhitespace(await readAndExtractTextFromS3(process.env.S3_BUCKET_NAME, resumeS3Key));
+    const jobDescriptionText = removeStopWordsAndExtraWhitespace(await readAndExtractTextFromS3(process.env.S3_BUCKET_NAME, jobDescriptionS3Key));
 
     // Get the number of questions from the request
     const numberOfQuestions = req.body.numberOfQuestions || 5; // Use the provided number or default to 5
