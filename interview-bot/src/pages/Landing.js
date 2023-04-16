60367a5433d9edafe38036ddade1e225093b40a7
colin baird <colinbaird89@gmail.com>
2023-04-16 16:45:25 -0500

    blocks animation sizing changes

--- interview-bot/src/pages/Landing.js
+++ interview-bot/src/pages/Landing.js
@@ -31,7 +31,7 @@ const Landing = () => {
         </div>
       </div>
       {/* Add 'hero-figure' class to the container */}
-      <div className="box-animation-container hero-figure" style={{ top: '10%', left: '10%' }}> {/* Add top and left styles */}
+      <div className="box-animation-container hero-figure anime-element" style={{ top: '10%', right: '10%' }}>
         <div className="hero-figure-box hero-figure-box-01" data-rotation="45deg"></div>
         <div className="hero-figure-box hero-figure-box-02" data-rotation="-45deg"></div>
         <div className="hero-figure-box hero-figure-box-03" data-rotation="0deg"></div>
