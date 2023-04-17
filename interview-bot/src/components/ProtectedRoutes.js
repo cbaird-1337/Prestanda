382c7debc9b748f5e27e46a8183a87d67a37c0b6
colin baird <colinbaird89@gmail.com>
2023-04-16 21:38:00 -0500

    updates to login and protected routes to pass user state upon login

--- interview-bot/src/components/ProtectedRoutes.js
+++ interview-bot/src/components/ProtectedRoutes.js
@@ -1,19 +1,9 @@
-// ProtectedRoutes.js
 import React, { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import { AccountContext } from './auth/Account';
 
 const ProtectedRoutes = ({ Component }) => {
-  console.log("AccountContext:", AccountContext); // Add this line
-  const { getSession } = useContext(AccountContext);
-
-  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
-
-  React.useEffect(() => {
-    getSession()
-      .then(() => setIsLoggedIn(true))
-      .catch(() => setIsLoggedIn(false));
-  }, [getSession]);
+  const { getSession, isLoggedIn } = useContext(AccountContext); // Add isLoggedIn here
 
   console.log("isLoggedIn:", isLoggedIn); // Add this line
 
