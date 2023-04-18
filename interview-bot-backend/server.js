f60de0060eded098eb095f6132d6161d147caa65
colin baird <colinbaird89@gmail.com>
2023-04-18 11:37:10 -0500

    more backend port updates

--- interview-bot-backend/server.js
+++ interview-bot-backend/server.js
@@ -210,7 +210,7 @@ app.post('/refine', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
