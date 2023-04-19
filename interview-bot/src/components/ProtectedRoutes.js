968ba630cad78f145262e09dbcdf382e1ee6270b
colin baird <colinbaird89@gmail.com>
2023-04-19 16:27:42 -0500

    updated ProtectedRoutes to handle page refresh

--- interview-bot/src/components/ProtectedRoutes.js
+++ interview-bot/src/components/ProtectedRoutes.js
@@ -1,18 +1,28 @@
 // ProtectedRoutes.js
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Navigate } from 'react-router-dom';
 import { AccountContext } from './auth/Account';
 
 const ProtectedRoutes = ({ Component }) => {
-  const { getSession, isLoggedIn } = useContext(AccountContext); // Add isLoggedIn
+  const { getSession, isLoggedIn } = useContext(AccountContext);
+  const [loading, setLoading] = useState(true);
 
   React.useEffect(() => {
     getSession()
-      .then(() => console.log('Session exists'))
-      .catch(() => console.log('No session'));
+      .then(() => {
+        console.log('Session exists');
+        setLoading(false);
+      })
+      .catch(() => {
+        console.log('No session');
+        setLoading(false);
+      });
   }, [getSession]);
 
-  // Render the passed Component directly
+  if (loading) {
+    return <div>Loading...</div>; // You can replace this with a proper loading component
+  }
+
   return isLoggedIn ? <Component /> : <Navigate to="/" />;
 };
 
