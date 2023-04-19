b59e783ec06020d4abcc01079bef7d4fbe5d70f0
colin baird <colinbaird89@gmail.com>
2023-04-19 10:24:12 -0500

    Updated PublicRoutes to use accountContext

--- interview-bot/src/components/PublicRoutes.js
+++ interview-bot/src/components/PublicRoutes.js
@@ -1,21 +1,10 @@
-// PublicRoutes.js
 import React, { useContext } from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { AccountContext } from './auth/Account';
 import Landing from '../pages/Landing';
 
-const useCheckSession = (accountContext) => {
-  const user = localStorage.getItem('user');
-  if (user) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
 const PublicRoutes = () => {
-  const accountContext = useContext(AccountContext);
-  const isLoggedIn = useCheckSession(accountContext);
+  const { isLoggedIn } = useContext(AccountContext);
 
   return isLoggedIn ? <Navigate to="/app" /> : <Landing />;
 };
