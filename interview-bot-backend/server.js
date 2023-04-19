ba7d2683d97a3e045627c78afa0e09b346dfab61
colin baird <colinbaird89@gmail.com>
2023-04-19 10:42:59 -0500

    backend CORS update

--- interview-bot-backend/server.js
+++ interview-bot-backend/server.js
@@ -42,7 +42,13 @@ const allowedOrigins = [
 ];
 
 const corsOptions = {
-  origin: true,
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
 };
 
