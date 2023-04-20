3c0d11e4792daa2aafa408ee4dbf6ec06e095406
colin baird <colinbaird89@gmail.com>
2023-04-20 09:05:30 -0500

    updated banner to new code

--- interview-bot/src/components/Banner.js
+++ interview-bot/src/components/Banner.js
@@ -1,10 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Banner.css";
+import { createStyles, Header, Group, Center, Container, rem } from "@mantine/core";
 import { AccountContext } from "./auth/Account";
+
+const useStyles = createStyles((theme) => ({
+  header: {
+    backgroundColor: theme.fn.variant({ variant: "filled", color: theme.primaryColor }).background,
+    borderBottom: 0,
+  },
+
+  inner: {
+    height: rem(56),
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+  link: {
+    display: "block",
+    lineHeight: 1,
+    padding: `${rem(8)} ${rem(12)}`,
+    borderRadius: theme.radius.sm,
+    textDecoration: "none",
+    color: theme.white,
+    fontSize: theme.fontSizes.sm,
+    fontWeight: 500,
+
+    "&:hover": {
+      backgroundColor: theme.fn.lighten(
+        theme.fn.variant({ variant: "filled", color: theme.primaryColor }).background || "",
+        0.1
+      ),
+    },
+  },
+}));
 
 function Banner() {
   const { logout } = React.useContext(AccountContext);
+  const { classes } = useStyles();
 
   const handleLogoutClick = (event) => {
     event.preventDefault();
@@ -16,19 +49,30 @@ function Banner() {
   };
 
   return (
-    <div className="banner">
-      <div>
-        <Link to="/account" className="banner-link">
-          Account
-        </Link>
-        <Link to="/interview-history" className="banner-link">
-          Interview History
-        </Link>
-        <a href="/" className="banner-link" onClick={handleLogoutClick}>
-          Logout
-        </a>
-      </div>
-    </div>
+    <Header height={56} className={classes.header}>
+      <Container>
+        <div className={classes.inner}>
+          {/* Add your logo here */}
+          <Group spacing={5}>
+            <Link to="/account" className={classes.link}>
+              Account
+            </Link>
+            <Link to="/interview-history" className={classes.link}>
+              Interview History
+            </Link>
+            <a href="/pricing" className={classes.link}>
+              Pricing
+            </a>
+            <a href="/support" className={classes.link}>
+              Support
+            </a>
+            <a href="/" className={classes.link} onClick={handleLogoutClick}>
+              Logout
+            </a>
+          </Group>
+        </div>
+      </Container>
+    </Header>
   );
 }
 
