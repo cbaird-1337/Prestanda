d49fe892ff119d88529151c71206baf2b80f40b3
colin baird <colinbaird89@gmail.com>
2023-04-21 13:51:36 -0500

    updates to landing css and containers

--- interview-bot/src/pages/Landing.js
+++ interview-bot/src/pages/Landing.js
@@ -19,33 +19,35 @@ const Landing = () => {
     <div className="App">
       <div className="bg-shapes"></div>
       <div className="centered-container">
-        <div className="login-container">
-          <div className="login-header">
-            <h1>Welcome to Prestanda's Automated Interview Screening Service</h1>
-            <p>Please create an account or login to proceed.</p>
+        <div className="login-content-wrapper">
+          <div className="login-container">
+            <div className="login-header">
+              <h1>Welcome to Prestanda's Automated Interview Screening Service</h1>
+              <p>Please create an account or login to proceed.</p>
+            </div>
+            <div className="button-group">
+              <Link to="/Signup" className="landing-button signup-button">
+                Create an Account
+              </Link>
+              <Link to="/login" className="landing-button login-button">
+                Login
+              </Link>
+            </div>
           </div>
-          <div className="button-group">
-            <Link to="/Signup" className="landing-button signup-button">
-              Create an Account
-            </Link>
-            <Link to="/login" className="landing-button login-button">
-              Login
-            </Link>
-          </div>
-        </div>
-        <div className="content-container">
-          {/* Add 'hero-figure' class to the container */}
-          <div className="box-animation-container hero-figure anime-element">
-            <div className="hero-figure-box hero-figure-box-01" data-rotation="45deg"></div>
-            <div className="hero-figure-box hero-figure-box-02" data-rotation="-45deg"></div>
-            <div className="hero-figure-box hero-figure-box-03" data-rotation="0deg"></div>
-            <div className="hero-figure-box hero-figure-box-04" data-rotation="-135deg"></div>
-            <div className="hero-figure-box hero-figure-box-05"></div>
-            <div className="hero-figure-box hero-figure-box-06"></div>
-            <div className="hero-figure-box hero-figure-box-07"></div>
-            <div className="hero-figure-box hero-figure-box-08" data-rotation="-22deg"></div>
-            <div className="hero-figure-box hero-figure-box-09" data-rotation="-52deg"></div>
-            <div className="hero-figure-box hero-figure-box-10" data-rotation="-50deg"></div>
+          <div className="content-container">
+            {/* Add 'hero-figure' class to the container */}
+            <div className="box-animation-container hero-figure anime-element">
+              <div className="hero-figure-box hero-figure-box-01" data-rotation="45deg"></div>
+              <div className="hero-figure-box hero-figure-box-02" data-rotation="-45deg"></div>
+              <div className="hero-figure-box hero-figure-box-03" data-rotation="0deg"></div>
+              <div className="hero-figure-box hero-figure-box-04" data-rotation="-135deg"></div>
+              <div className="hero-figure-box hero-figure-box-05"></div>
+              <div className="hero-figure-box hero-figure-box-06"></div>
+              <div className="hero-figure-box hero-figure-box-07"></div>
+              <div className="hero-figure-box hero-figure-box-08" data-rotation="-22deg"></div>
+              <div className="hero-figure-box hero-figure-box-09" data-rotation="-52deg"></div>
+              <div className="hero-figure-box hero-figure-box-10" data-rotation="-50deg"></div>
+            </div>
           </div>
         </div>
       </div>
