d7d43360c2cf0cc241032c331cd332a7daf0a9d5
colin baird <colinbaird89@gmail.com>
2023-04-25 14:19:15 -0500

    more pricing card updates

--- interview-bot/src/pages/elements/PricingCards.js
+++ interview-bot/src/pages/elements/PricingCards.js
@@ -9,12 +9,12 @@ const PricingCards = () => {
         <div className="bg-gradient-to-br from-[#0270d7] via-[#0270d7] to-[#004a9e] shadow-lg rounded-lg px-8 pt-8 pb-10 text-center w-[20rem] h-[28rem] flex flex-col justify-between"> {/* Increase height and add flex classes */}
           <h2 className="text-2xl font-semibold mb-4 text-[#c0b081]">Free</h2>
           <div className="flex-grow"> 
-           <ul className="list-none space-y-2 text-[#dcd0d0] w-full">
-            <li className="text-base"><strong>Our default 'free forever' plan:</strong></li>
-            <li className="text-base">* 5 Automated interviews per month</li>
-            <li className="text-base">* 5 Psychometric assessments per month</li>
+           <ul className="list-none space-y-2 text-[#dcd0d0] w-full" style={{listStyleType: 'disc'}}>
+            <li className="text-base text-black"><strong>Our default 'free forever' plan:</strong></li>
+            <li className="text-base">* 5 Automated interviews /month</li>
+            <li className="text-base">* 5 Psychometric assessments /month</li>
             <li className="text-base">* Job fit recommendations for each interview</li>
-            <li className="text-base">* Psychometric assessment report</li>
+            <li className="text-base">* Psychometric assessment reports</li>
           </ul>
           </div>
           <button className="bg-[#444444] text-white font-semibold py-2 px-4 rounded-md hover:bg-[#666666]">
@@ -26,10 +26,10 @@ const PricingCards = () => {
         <div className="bg-gradient-to-br from-[#0270d7] via-[#0270d7] to-[#004a9e] shadow-lg rounded-lg px-8 pt-8 pb-10 text-center w-[20rem] h-[28rem] flex flex-col justify-between"> {/* Increase height and add flex classes */}
           <h2 className="text-2xl font-semibold mb-4 text-[#c0b081]">Pay as you Go</h2>
           <div className="flex-grow"> 
-           <ul className="list-none space-y-2 text-[#dcd0d0] w-full">
-            <li className="text-base"><strong>No need for contracts or commitments, only pay for what you use, as you use it:</strong></li>
-            <li className="text-base">* $2 per interview screening</li>
-            <li className="text-base">* $4 per psychometric assessment</li>
+           <ul className="list-none space-y-2 text-[#dcd0d0] w-full" style={{listStyleType: 'disc'}}>
+            <li className="text-base text-black"><strong>No need for contracts or commitments, only pay for what you use, as you use it:</strong></li>
+            <li className="text-base">* $2 /interview screening</li>
+            <li className="text-base">* $4 /psychometric assessment</li>
             <li className="text-base">* Job fit recommendations for each interview</li>
             <li className="text-base">* Psychometric assessment report</li>
           </ul>
@@ -43,10 +43,10 @@ const PricingCards = () => {
         <div className="bg-gradient-to-br from-[#0270d7] via-[#0270d7] to-[#004a9e] shadow-lg rounded-lg px-8 pt-8 pb-10 text-center w-[20rem] h-[28rem] flex flex-col justify-between"> {/* Increase height and add flex classes */}
           <h2 className="text-2xl font-semibold mb-4 text-[#c0b081]">Monthly or Annual Commit</h2>
           <div className="flex-grow"> 
-           <ul className="list-none space-y-2 text-[#dcd0d0] w-full">
-            <li className="text-base"><strong>Looking for more flexibility with higher screening volumes? Contact us to inquire about custom pricing:</strong></li>
-            <li className="text-base">As low as $0.95 per interview screening</li>
-            <li className="text-base">As low as $2.95 per psychometric assessment</li>
+           <ul className="list-none space-y-2 text-[#dcd0d0] w-full" style={{listStyleType: 'disc'}}>
+            <li className="text-base text-black"><strong>Looking for more flexibility with higher screening volumes? Contact us to inquire about custom pricing:</strong></li>
+            <li className="text-base">* As low as $0.95 /interview screening</li>
+            <li className="text-base">* As low as $2.95 /psychometric assessment</li>
           </ul>
           </div>
           <button className="bg-[#444444] text-white font-semibold py-2 px-4 rounded-md hover:bg-[#666666]">
