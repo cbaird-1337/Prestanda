c5e59f87a2745cd3939a413115432259620864d2
colin baird <colinbaird89@gmail.com>
2023-04-25 14:50:23 -0500

    ok I lied, but now Im done with the cards! 4real

--- interview-bot/src/pages/elements/PricingCards.js
+++ interview-bot/src/pages/elements/PricingCards.js
@@ -9,17 +9,14 @@ const PricingCards = () => {
         {/* Card 1 */}
         <div className="bg-gradient-to-br from-[#0270d7] via-[#0270d7] to-[#004a9e] shadow-lg rounded-sm px-8 pt-8 pb-10 text-center w-[20rem] h-[28rem] flex flex-col justify-between"> {/* Increase height and add flex classes */}
           <h2 className="text-3xl font-semibold mb-4 text-[#c0b081]">Free</h2>
-          <div className="flex-grow"> 
-           <ul className="list-none space-y-2 text-[#dcd0d0] w-full">
-            <li className="text-base text-black"><strong>Our default, free forever plan:</strong></li>
-           </ul>
-           <ul className="list-none space-y-2 text-[#dcd0d0] w-full" style={{listStyleType: 'disc'}}>
-            <li className="text-base"> 5 Automated interviews per month</li>
-            <li className="text-base"> 5 Psychometric assessments per month</li>
-            <li className="text-base"> Candidate job fit recommendations</li>
-            <li className="text-base"> Psychometric assessment reports</li>
-          </ul>
-          </div>
+          <div className="flex-grow flex items-center justify-center">
+            <ul className="list-none space-y-2 text-[#dcd0d0] w-full" style={{listStyleType: 'disc'}}>
+                <li className="text-base"> 5 Automated screening calls per month</li>
+                <li className="text-base"> 5 Psychometric assessments per month</li>
+                <li className="text-base"> Job fit recommendations</li>
+                <li className="text-base"> Psychometric assessment reports</li>
+            </ul>
+            </div>
           <Link to="/Signup" className="bg-[#444444] text-white font-semibold py-2 px-4 rounded-md hover:bg-[#666666] block text-center">
              Get Started
           </Link>
@@ -28,17 +25,14 @@ const PricingCards = () => {
         {/* Card 2 */}
         <div className="bg-gradient-to-br from-[#0270d7] via-[#0270d7] to-[#004a9e] shadow-lg rounded-sm px-8 pt-8 pb-10 text-center w-[20rem] h-[28rem] flex flex-col justify-between"> {/* Increase height and add flex classes */}
           <h2 className="text-3xl font-semibold mb-4 text-[#c0b081]">Pay as you Go</h2>
-          <div className="flex-grow"> 
-           <ul className="list-none space-y-2 text-[#dcd0d0] w-full">
-            <li className="text-base text-black"><strong>No need for contracts or commitments. Only pay for what you use, as you use it:</strong></li>
-           </ul>
-           <ul className="list-none space-y-2 text-[#dcd0d0] w-full" style={{listStyleType: 'disc'}}>
-            <li className="text-base"> $2 per interview screening</li>
-            <li className="text-base"> $4 per psychometric assessment</li>
-            <li className="text-base"> Candidate job fit recommendations</li>
-            <li className="text-base"> Psychometric assessment report</li>
-          </ul>
-          </div>
+          <div className="flex-grow flex items-center justify-center">
+            <ul className="list-none space-y-2 text-[#dcd0d0] w-full" style={{listStyleType: 'disc'}}>
+                <li className="text-base"> $1.95 per interview screening call</li>
+                <li className="text-base"> $3.95 per psychometric assessment</li>
+                <li className="text-base"> Job fit recommendations</li>
+                <li className="text-base"> Psychometric assessment report</li>
+            </ul>
+            </div>
           <Link to="/Signup" className="bg-[#444444] text-white font-semibold py-2 px-4 rounded-md hover:bg-[#666666] block text-center">
             Get Started
           </Link>
@@ -47,15 +41,12 @@ const PricingCards = () => {
         {/* Card 3 */}
         <div className="bg-gradient-to-br from-[#0270d7] via-[#0270d7] to-[#004a9e] shadow-lg rounded-sm px-8 pt-8 pb-10 text-center w-[20rem] h-[28rem] flex flex-col justify-between"> {/* Increase height and add flex classes */}
           <h2 className="text-3xl font-semibold mb-4 text-[#c0b081]">Monthly or Annual Commit</h2>
-          <div className="flex-grow"> 
-           <ul className="list-none space-y-2 text-[#dcd0d0] w-full">
-            <li className="text-base text-black"><strong>Looking for more flexibility with higher screening volumes? Contact us below:</strong></li>
-           </ul>
-           <ul className="list-none space-y-2 text-[#dcd0d0] w-full" style={{listStyleType: 'disc'}}>
-            <li className="text-base"> As low as $0.95 per interview screening</li>
-            <li className="text-base"> As low as $2.95 per psychometric assessment</li>
-          </ul>
-          </div>
+          <div className="flex-grow flex items-center justify-center">
+            <ul className="list-none space-y-2 text-[#dcd0d0] w-full" style={{listStyleType: 'disc'}}>
+                <li className="text-base"> As low as $1 per interview screening</li>
+                <li className="text-base"> As low as $2.50 per psychometric assessment</li>
+            </ul>
+            </div>
           <button className="bg-[#444444] text-white font-semibold py-2 px-4 rounded-md hover:bg-[#666666]">
             Inquire for Details
           </button>
