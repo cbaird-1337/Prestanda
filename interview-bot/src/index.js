95f319d84cd5678e0b4913f4ee20127ca93b3459
colin baird <colinbaird89@gmail.com>
2023-04-25 20:46:49 -0500

    config changes for tailwind

--- interview-bot/src/index.js
+++ interview-bot/src/index.js
@@ -4,8 +4,6 @@ import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import "@mantine/core/lib/index.css";
-import "./tailwind.css";
 
 ReactDOM.render(
   <React.StrictMode>
--- interview-bot/tailwind.config.js
+++ interview-bot/tailwind.config.js
@@ -1,27 +1,26 @@
 // tailwind.config.js
 module.exports = {
-    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    darkMode: 'media', // 'media' or 'class'
-    theme: {
-      extend: {
-        fontFamily: {
-          sans: ['Roboto', 'ui-sans-serif', 'system-ui'],
-        },
-        backgroundColor: {
-          dark: '#1f1f1f',
-          accent: '#007bff',
-        },
-        borderColor: {
-          dark: '#444',
-        },
-        backgroundImage: {
-          'gradient': 'linear-gradient(135deg, #0b132b 0%, #3a506b 100%)',
-        },
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'media', // 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'ui-sans-serif', 'system-ui'],
+      },
+      backgroundColor: {
+        dark: '#1f1f1f',
+        accent: '#007bff',
+      },
+      borderColor: {
+        dark: '#444',
+      },
+      backgroundImage: {
+        'gradient': 'linear-gradient(135deg, #0b132b 0%, #3a506b 100%)',
       },
     },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
