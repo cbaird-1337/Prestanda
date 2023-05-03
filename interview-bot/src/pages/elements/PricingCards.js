b2d6bf176ce88cafeb0f36a56d3d64af9b5ae696
colin baird <colinbaird89@gmail.com>
2023-05-03 10:48:43 -0500

    more landing css updates

--- interview-bot/src/pages/elements/PricingCards.js
+++ interview-bot/src/pages/elements/PricingCards.js
@@ -8,13 +8,13 @@ const PricingCards = () => {
       <div className="flex flex-col sm:flex-row justify-center space-y-4 sm:space-y-0 sm:space-x-8 z-50">
         {/* Card 1 */}
         <div className="bg-gradient-to-br from-[#0270d7] via-[#0270d7] to-[#004a9e] shadow-lg rounded-sm px-8 pt-8 pb-10 text-center w-[20rem] lg:w-[22rem] xl:w-[24rem] 2xl:w-[28rem] h-[28rem] lg:h-[32rem] xl:h-[36rem] 2xl:h-[40rem] flex flex-col justify-between"> {/* Increase height and add flex classes */}
-          <h2 className="text-3xl font-semibold mb-4 text-[#c0b081]">FREE</h2>
+        <h2 className="text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-semibold mb-4 text-[#c0b081]">FREE</h2>
           <div className="flex-grow flex items-center justify-center">
             <ul className="list-none space-y-2 text-[#dcd0d0] w-full" style={{listStyleType: 'disc'}}>
-                <li className="text-base"> 5 Automated screening calls /month</li>
-                <li className="text-base"> 5 Psychometric assessments /month</li>
-                <li className="text-base"> Job fit recommendations</li>
-                <li className="text-base"> Psychometric assessment reports</li>
+                <li className="text-base lg:text-lg xl:text-xl 2xl:text-2xl"> 5 Automated screening calls /month</li>
+                <li className="text-base lg:text-lg xl:text-xl 2xl:text-2xl"> 5 Psychometric assessments /month</li>
+                <li className="text-base lg:text-lg xl:text-xl 2xl:text-2xl"> Job fit recommendations</li>
+                <li className="text-base lg:text-lg xl:text-xl 2xl:text-2xl"> Psychometric assessment reports</li>
             </ul>
             </div>
           <Link to="/Signup" className="bg-[#444444] text-white font-semibold py-2 px-4 rounded-md hover:bg-[#666666] block text-center">
@@ -24,13 +24,13 @@ const PricingCards = () => {
 
         {/* Card 2 */}
         <div className="bg-gradient-to-br from-[#0270d7] via-[#0270d7] to-[#004a9e] shadow-lg rounded-sm px-8 pt-8 pb-10 text-center w-[20rem] lg:w-[22rem] xl:w-[24rem] 2xl:w-[28rem] h-[28rem] lg:h-[32rem] xl:h-[36rem] 2xl:h-[40rem] flex flex-col justify-between"> {/* Increase height and add flex classes */}
-          <h2 className="text-3xl font-semibold mb-4 text-[#c0b081]">Pay As You Go</h2>
+        <h2 className="text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-semibold mb-4 text-[#c0b081]">Pay As You Go</h2>
           <div className="flex-grow flex items-center justify-center">
             <ul className="list-none space-y-2 text-[#dcd0d0] w-full" style={{listStyleType: 'disc'}}>
-                <li className="text-base"> $1.95 per automated screening call</li>
-                <li className="text-base"> $3.95 per psychometric assessment</li>
-                <li className="text-base"> Job fit recommendations</li>
-                <li className="text-base"> Psychometric assessment report</li>
+                <li className="text-base lg:text-lg xl:text-xl 2xl:text-2xl"> $1.95 per automated screening call</li>
+                <li className="text-base lg:text-lg xl:text-xl 2xl:text-2xl"> $3.95 per psychometric assessment</li>
+                <li className="text-base lg:text-lg xl:text-xl 2xl:text-2xl"> Job fit recommendations</li>
+                <li className="text-base lg:text-lg xl:text-xl 2xl:text-2xl"> Psychometric assessment report</li>
             </ul>
             </div>
           <Link to="/Signup" className="bg-[#444444] text-white font-semibold py-2 px-4 rounded-md hover:bg-[#666666] block text-center">
@@ -40,12 +40,12 @@ const PricingCards = () => {
 
         {/* Card 3 */}
         <div className="bg-gradient-to-br from-[#0270d7] via-[#0270d7] to-[#004a9e] shadow-lg rounded-sm px-8 pt-8 pb-10 text-center w-[20rem] lg:w-[22rem] xl:w-[24rem] 2xl:w-[28rem] h-[28rem] lg:h-[32rem] xl:h-[36rem] 2xl:h-[40rem] flex flex-col justify-between"> {/* Increase height and add flex classes */}
-          <h2 className="text-3xl font-semibold mb-4 text-[#c0b081]">Monthly or Annual Commit</h2>
+        <h2 className="text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-semibold mb-4 text-[#c0b081]">Monthly or Annual Commit</h2>
           <div className="flex-grow flex items-center justify-center">
             <ul className="list-none space-y-2 text-[#dcd0d0] w-full" style={{listStyleType: 'disc'}}>
-                <li className="text-base"> Discounts applied in per-month increments of 250 calls or assessments</li>
-                <li className="text-base"> As low as $1 per call screening</li>
-                <li className="text-base"> As low as $3 per psychometric assessment</li>
+                <li className="text-base lg:text-lg xl:text-xl 2xl:text-2xl"> Discounts applied in per-month increments of 250 calls or assessments</li>
+                <li className="text-base lg:text-lg xl:text-xl 2xl:text-2xl"> As low as $1 per call screening</li>
+                <li className="text-base lg:text-lg xl:text-xl 2xl:text-2xl"> As low as $3 per psychometric assessment</li>
             </ul>
             </div>
           <button className="bg-[#444444] text-white font-semibold py-2 px-4 rounded-md hover:bg-[#666666]">
