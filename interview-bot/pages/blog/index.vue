57fac444c648f444b667b6c24009b8756f94051d
colin baird <colinbaird89@gmail.com>
2023-08-31 17:48:32 -0500

    Blog page

--- interview-bot/pages/blog/index.vue
+++ interview-bot/pages/blog/index.vue
@@ -1,19 +1,38 @@
 <template>
-    <div>
-      <h1>Blogs Page</h1>
-      <!-- Add your blog content or other components here -->
+  <div class="bg-base-100 p-4">
+    <h1 class="text-3xl font-bold mb-4 text-neutral">Blogs Page</h1>
+    <div class="card">
+      <div 
+        id="retainable-rss-embed" 
+        data-rss="https://medium.com/feed/@prestanda.io" 
+        data-maxcols="3" 
+        data-layout="grid" 
+        data-poststyle="inline" 
+        data-readmore="Read the rest" 
+        data-buttonclass="btn btn-primary" 
+        data-offset="-100"
+      >
+      </div>
     </div>
-  </template>
-  
-  <script>
-  // If you want to add any methods, data, computed properties, etc., add them here
-  export default {
-    name: 'BlogsPage',
-    // other Vue component options
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    let rssScript = document.createElement('script');
+    rssScript.setAttribute('src', 'https://www.twilik.com/assets/retainable/rss-embed/retainable-rss-embed.js');
+    document.head.appendChild(rssScript);
   }
-  </script>
-  
-  <style scoped>
-  /* Add your CSS styling for this page here, 'scoped' ensures it only applies to this component */
-  </style>
-  
+}
+</script>
+
+<style scoped>
+.card {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+}
+
+</style>
